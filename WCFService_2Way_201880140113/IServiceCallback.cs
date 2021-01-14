55a249cb61051e5bf8bfd403f20e1b001c026c73
Bambang Marsudi <bambang.marsudi.ft18@mail.umy.ac.id>
2021-01-14 14:18:10 +0700

    clear service

---
+++ WCFService_2Way_201880140113/IServiceCallback.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFService_2Way_201880140113
+{
+   
+    [ServiceContract(CallbackContract = typeof(IClientCallback))]
+    public interface IServiceCallback
+    {
+
+        [OperationContract(IsOneWay = true)]
+        void gabung(string username);
+
+        [OperationContract(IsOneWay = true)]
+        void kirimPesan(string pesan);
+    }
+    [ServiceContract]
+    interface IClientCallback
+    {
+        [OperationContract(IsOneWay = true)]
+        void pesanKirim(string user, string pesan);
+    }
+
+    
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+}
---
+++ WCFService_2Way_201880140113/ServiceCallback.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCFService_2Way_201880140113
+{
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Single, InstanceContextMode = InstanceContextMode.Single)]
+    public class ServiceCallback : IServiceCallback
+    {
+        Dictionary<IClientCallback, string> userlist = new Dictionary<IClientCallback, string>(); // Menyimpan data ketika user online
+
+        public void gabung(string username)
+        {
+            IClientCallback koneksiGabung = OperationContext.Current.GetCallbackChannel<IClientCallback>(); // untuk menampung user ketika baru daftar / buat akun
+            userlist[koneksiGabung] = username;
+        }
+
+        public void kirimPesan(string pesan)
+        {
+            IClientCallback koneksiPesan = OperationContext.Current.GetCallbackChannel<IClientCallback>(); // mengirim data user dan pesan ke user lain
+
+            string user;
+            if (!userlist.TryGetValue(koneksiPesan, out user))
+            {
+                return;
+            }
+            foreach (IClientCallback other in userlist.Keys)
+            {
+                other.pesanKirim(user, pesan);
+            }
+        }
+    }
+}
